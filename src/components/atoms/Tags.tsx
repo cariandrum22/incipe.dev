9cad0be57daea07e8963632d2f5fd100b3792dbd
Takafumi Asano <cariandrum22@gmail.com>
2023-08-07 14:36:13 +0900

    Fix a problem where multiple tags were concatenated

--- src/components/atoms/Tags.tsx
+++ src/components/atoms/Tags.tsx
@@ -7,10 +7,13 @@ type Props = {
 const Tags: React.FC<Props> = ({ tags }) => (
   <small className="inline font-thin text-slate-700">
     Tags:&nbsp;
-    {tags.map(tag => (
-      <a className="text-indigo-600" href={`/blog/tag/${tag}`} key={tag}>
-        {tag}
-      </a>
+    {tags.map((tag, index) => (
+      <React.Fragment key={tag}>
+        <a className="text-indigo-600" href={`/blog/tag/${tag}`}>
+          {tag}
+        </a>
+        {index !== tags.length - 1 && " "}
+      </React.Fragment>
     ))}
   </small>
 )

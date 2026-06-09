0d60f6bddaab1b63dc1396a7d3b3616c8609233a
Takafumi Asano <cariandrum22@gmail.com>
2026-06-09 12:56:53 +0900

    chore: remove gatsby image plugins (#37)

--- src/components/atoms/Image.tsx
+++ src/components/atoms/Image.tsx
@@ -1,9 +1,7 @@
 import React from "react"
-import { GatsbyImage, getSrc } from "gatsby-plugin-image"
-import type { IGatsbyImageData } from "gatsby-plugin-image"
 import type { ContentImage } from "../../types/content"
 
-export type ImageData = IGatsbyImageData | ContentImage
+export type ImageData = ContentImage
 
 type Props = {
   alt: string
@@ -11,19 +9,17 @@ type Props = {
   image: ImageData
 }
 
-const isGatsbyImageData = (image: ImageData): image is IGatsbyImageData =>
-  "images" in image && "layout" in image
+const Image: React.FC<Props> = ({ alt, className, image }) => (
+  <img
+    alt={alt}
+    className={className}
+    height={image.height ?? undefined}
+    src={image.url}
+    width={image.width ?? undefined}
+  />
+)
 
-const Image: React.FC<Props> = ({ alt, className, image }) => {
-  if (isGatsbyImageData(image)) {
-    return <GatsbyImage alt={alt} className={className} image={image} />
-  }
-
-  return <img alt={alt} className={className} src={image.url} />
-}
-
-const getImageSrc = (image: ImageData) =>
-  isGatsbyImageData(image) ? getSrc(image) : image.url
+const getImageSrc = (image: ImageData) => image.url
 
 export { getImageSrc }
 export default Image
